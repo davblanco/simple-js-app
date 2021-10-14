5efb25e240f22357d6d6a4d444e35ee65db45ab5
davblanco <davblanco703@gmail.com>
2021-10-13 20:43:28 -0400

    Added 4 pokemons
    also added 3 traits to  go along with it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,36 @@
-alert('Hello Wworld');
+console.log('Hello World');
 
-let favorite_food = 'burgers';
-document.write(favorite_food);s
+var pokemonist = [
+  {
+		name: 'Bulbasaur',
+		height: '.7m',
+    type:['grass', 'poison'],
+
+		},
+
+    {
+      name: 'Carmander',
+      height: '.6m',
+      type:['fire'],
+  
+      },
+
+      {
+        name: 'Squirtle',
+        height: '.5m',
+        type:['water'],
+    
+        },
+
+        {
+          name: 'pidgey',
+          height: '.3m',
+          type:['normal , flying'],
+      
+          }
+      
+    
+  
+
+];
+
