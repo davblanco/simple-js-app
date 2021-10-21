fdddc0b133e8183264cff562a6946d9f15d266f7
davblanco <davblanco703@gmail.com>
2021-10-21 09:14:16 -0400

    gave the title "pokedex"

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,30 @@
-console.log('Hello World');
-
-var pokemonist = [
+let pokemonlist = [
   {
-		name: 'Bulbasaur',
-		height: '.7m',
-    type:['grass', 'poison'],
+    name: "Bulbasaur",
+    height: ".7m",
+    type: ["grass", "poison"],
+  },
 
-		},
+  {
+    name: "Carmander",
+    height: ".6m",
+    type: ["fire"],
+  },
 
-    {
-      name: 'Carmander',
-      height: '.6m',
-      type:['fire'],
-  
-      },
+  {
+    name: "Squirtle",
+    height: ".5m",
+    type: ["water"],
+  },
 
-      {
-        name: 'Squirtle',
-        height: '.5m',
-        type:['water'],
-    
-        },
-
-        {
-          name: 'pidgey',
-          height: '.3m',
-          type:['normal , flying'],
-      
-          }
-      
-    
-  
-
+  {
+    name: "pidgey",
+    height: ".3m",
+    type: ["normal , flying"],
+  },
 ];
 
+for (i = 0; pokemonlist < pokemonlist.length; i++) {
+  document.write(
+    pokemonlist[i].name + " Height: " + pokemonlist[i].height + "<br>");
+}
